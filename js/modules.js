b242c50bb15a667935b9442f536dd5dd76aef4d8
Gordey <112198400+Finyi@users.noreply.github.com>
2023-06-09 11:34:35 +0300

    Delete modules.js

--- js/modules.js
+++
@@ -1,8 +0,0 @@
-/* Маркер сообщения */
-L.marker(L.latLng(49.33586, 49.33586), {
-    icon: L.icon({
-        iconUrl: 'img/rus/22-k-p.png',   // url картинки
-        iconSize: [40, 40],   // размер маркера
-        iconAnchor: [20, 20],   // выравнивание относительно центра
-    }),
-}).addTo(map);
